0875c9f90e1c59e7c578fa522436d4d191605917
Choua Vue <vuec23@gmail.com>
2022-04-13 23:11:25 -0400

    testing

---
+++ _tests_/myscript.spec.js
@@ -0,0 +1,28 @@
+const { Hand, playRound , playUntill } = require("../src/myscript");
+
+
+test('testing Hands to be rock, paper, scissor', () => {
+    expect(Hand).toEqual(['rock', 'paper', 'scissors'])
+});
+
+test('testing lenght of Hands to be 3', () => {
+    expect(Hand).toHaveLength(3)
+});
+
+test('testing playUntil is 5', () => {
+    expect(playUntill).toBeTruthy()
+});
+
+
+test('testing playRound', () => {
+    const playRound = jest.fn();
+    playRound();
+    expect(playRound).toHaveBeenCalled();
+    });
+
+test('testing playRound', () => {
+    const playRound = jest.fn(() => true);
+    playRound();
+    expect(playRound).toHaveReturned();
+});
+
